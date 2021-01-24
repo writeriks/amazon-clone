0f3ace4b3005e171cac3de28e42b8cfb5af777b6
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2021-01-24 12:58:43 +0100

    Install material ui and & add header component w style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon Clone 🚀🍀</h1>
+    <div className="app">
+      <Header />
+      {/* HOME */}
     </div>
   );
 }
