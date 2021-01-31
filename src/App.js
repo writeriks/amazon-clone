cf3d8eb73a86b9cd5b8359af5981a4487f8c4cf8
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2021-01-31 13:35:49 +0100

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './Header/Header';
+import Home from './Home/Home';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* HOME */}
+      <Home />
     </div>
   );
 }
