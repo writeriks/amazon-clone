9fcd37ac7dd80e2355b74cf2640160c17c6a5f2f
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2021-03-10 22:45:45 +0100

    Update payment helper

--- src/Payment/payment.jsx
+++ src/Payment/payment.jsx
@@ -7,22 +7,20 @@ import {CardElement, useElements, useStripe} from '@stripe/react-stripe-js';
 import CurrencyFormat from 'react-currency-format';
 import axios from '../axios';
 import paymentHelper from './payment-helper';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import authReducerSelector from '../redux-reducer/auth-reducer/auth-reducer-selector';
-import basketActionCreator from '../redux-reducer/basket-reducer/basket-action-creator';
 
 
 function Payment() {
     const history = useHistory()
 
-    const dispatch = useDispatch()
     const basket = useSelector(basketReducerSelector.getBasket)
     const basketTotal = useSelector(basketReducerSelector.getBasketTotal)
     const user = useSelector(authReducerSelector.getCurrentUser)
 
     const stripe = useStripe();
     const elements = useElements();
-    const {confirmPaymentWithoutSecret} = paymentHelper;
+    const {submitPayment} = paymentHelper;
 
     const [succeeded, setSucceeded] = useState(false);
     const [processing, setProcessing] = useState("");
@@ -31,11 +29,9 @@ function Payment() {
     const [clientSecret, setClientSecret] = useState(false)
 
     useEffect(() => {
-        // Get client Secret from Cloud Function
         const getClientSecret = async () => {
             const response = await axios({
                 method: 'post',
-                // Stripe expects the total in currencies subunits
                 url: `/payments/create?total=${basketTotal * 100}`
             })
             setClientSecret(response.data.clientSecret)
@@ -44,39 +40,25 @@ function Payment() {
     }, [basket, basketTotal])
 
 
-    /**
-     * creates request to stripe with received client secret from the useEffect
-     * and card element from react stripe
-     * 
-     * @param {*} e event object to prevent postback refreshing
-     */
+    const paymentParameters = {
+        stripe,
+        elements,
+        CardElement,
+        clientSecret,
+        user,
+        basket,
+        history,
+        setProcessing,
+        setSucceeded,
+        setError,
+    }
+
     const handlePaymentSubmit = async (e) => {
         e.preventDefault();
         if (!stripe || !elements) {
-            // Stripe.js has not loaded yet. Make sure to disable
-            // form submission until Stripe.js has loaded.
             return;
         }
-        setProcessing(true);
-        const cardElement = elements.getElement(CardElement);
-        if (clientSecret) {
-            await paymentHelper.confirmPaymentWithSecret(stripe, clientSecret, cardElement, user, basket)
-            setSucceeded(true)
-            setProcessing(false)
-            setError(null)
-            history.replace('/orders')
-        } else {
-            const {error, paymentMethod} = await confirmPaymentWithoutSecret(stripe, elements, cardElement)
-            setProcessing(false)
-            if (paymentMethod) {
-                setSucceeded(true)
-                setError(null)
-                history.replace('/orders')
-            } else {
-                setError(error.message)
-            }
-        }
-        dispatch(basketActionCreator.emptyBasketAction())
+        await submitPayment(paymentParameters)
     }
 
     const handlePaymentChange = (e) => {
